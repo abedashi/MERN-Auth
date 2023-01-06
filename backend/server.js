6d9d9889ecda26e3ca2416f16739317d0acaa4c9
Abed al salam Ashi <abed_ashi@hotmail.com>
2023-01-06 04:10:35 +0200

    server

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,18 @@
 const express = require('express');
+const colors = require('colors');
 const dotenv = require('dotenv').config();
-const port = process.env.PORT;
+const { errorHandler } = require('./middleWare/errorMiddleWare');
+const connectDB = require('./config/db');
+const port = process.env.PORT || 5001;
 
-const app = express();
+connectDB();
 
-app.get('/api/goals', (req, res) => {
-    res.json({ message: 'Get goals' })
-})
+const app = express()
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.listen(port, () => console.log(`Server started on port ${port}`));
+app.use('/api/users', require('./routes/userRoutes'));
+
+app.use(errorHandler);
+
+app.listen(port, () => console.log(`Server started on port ${port.yellow}`));
