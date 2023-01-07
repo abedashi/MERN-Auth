7522cdcd66926096136e552ef8fc23405ffa67c2
Abed al salam Ashi <abed_ashi@hotmail.com>
2023-01-07 20:47:22 +0200

    adding CORS

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const colors = require('colors');
 const dotenv = require('dotenv').config();
 const { errorHandler } = require('./middleWare/errorMiddleWare');
@@ -7,7 +8,8 @@ const port = process.env.PORT || 5001;
 
 connectDB();
 
-const app = express()
+const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
