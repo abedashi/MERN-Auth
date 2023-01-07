24dca8a4fce1e739cf2ad866953dc9bb2d657077
Abed al salam Ashi <abed_ashi@hotmail.com>
2023-01-07 20:45:05 +0200

    components

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,49 @@
+import { FaSignInAlt, FaSignOutAlt, FaUser } from "react-icons/fa";
+import { useSelector, useDispatch } from "react-redux";
+import { logout, reset } from "../features/auth/authSlice";
+import { Link, useNavigate } from "react-router-dom";
+
+const Header = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.auth);
+
+  const onLogouthandler = () => {
+    dispatch(logout());
+    dispatch(reset());
+    navigate("/");
+  };
+
+  return (
+    <header className="header">
+      <div className="logo">
+        <Link to="/">GoalSetter</Link>
+      </div>
+      <ul>
+        {!user && (
+          <>
+            <li>
+              <Link to="/login">
+                <FaSignInAlt /> Login
+              </Link>
+            </li>
+            <li>
+              <Link to="/register">
+                <FaUser /> Register
+              </Link>
+            </li>
+          </>
+        )}
+        {user && (
+          <li>
+            <button className="btn" onClick={onLogouthandler}>
+              <FaSignOutAlt /> Logout
+            </button>
+          </li>
+        )}
+      </ul>
+    </header>
+  );
+};
+
+export default Header;
